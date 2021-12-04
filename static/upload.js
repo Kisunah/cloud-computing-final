37bfb3c9250d81bd82c45e9c19c962ea5f185731
stsoward <darkarchonsc2@gmail.com>
2021-12-03 20:00:00 -0500

    Update upload.js

--- static/upload.js
+++ static/upload.js
@@ -9,4 +9,110 @@ let week_num = document.getElementById('week_num');
 let year = document.getElementById('year');
 let uploadButton = document.getElementById('uploadButton');
 
-//to be finished
+basket_num.addEventListener('input', (event) => {
+    if (event.target.value == '' || hshd_num.value == '' || purchase_.value == ''  || product_num.value == '' || spend.value == '' || units.value == '' || store_r.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+hshd_num.addEventListener('input', (event) => {
+    if (basket_num.value == '' || event.target.value == '' || purchase_.value == ''  || product_num.value == '' || spend.value == '' || units.value == '' || store_r.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+})
+
+purchase_.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || event.target.value == ''  || product_num.value == '' || spend.value == '' || units.value == '' || store_r.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+product_num.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || purchase_.value == ''  || event.target.value == '' || spend.value == '' || units.value == '' || store_r.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+spend.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || purchase_.value == ''  || product_num.value == '' || event.target.value == '' || units.value == '' || store_r.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+units.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || purchase_.value == ''  || product_num.value == '' || spend.value == '' || event.target.value == '' || store_r.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+store_r.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || purchase_.value == ''  || product_num.value == '' || spend.value == '' || units.value == '' || event.target.value == '' || week_num.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+week_num.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || purchase_.value == ''  || product_num.value == '' || spend.value == '' || units.value == '' || store_r.value == '' || event.target.value == '' || year.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
+
+year.addEventListener('input', (event) => {
+    if (basket_num.value == '' || hshd_num.value == '' || purchase_.value == ''  || product_num.value == '' || spend.value == '' || units.value == '' || store_r.value == '' || week_num.value == '' || event.target.value == '') {
+        uploadButton.style.pointerEvents = 'none';
+        uploadButton.style.cursor = 'default';
+        uploadButton.style.backgroundColor = 'grey';
+    } else {
+        uploadButton.style.pointerEvents = 'all';
+        uploadButton.style.cursor = 'pointer';
+        uploadButton.style.backgroundColor = '#E00122';
+    }
+});
