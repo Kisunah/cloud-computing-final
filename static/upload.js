02de27f57d83d6331ff7a4fbcf3ad9df06e69e33
stsoward <darkarchonsc2@gmail.com>
2021-12-03 19:24:58 -0500

    Create upload.js

---
+++ static/upload.js
@@ -0,0 +1,12 @@
+let basket_num = document.getElementById('basket_num');
+let hshd_num = document.getElementById('hshd_num');
+let purchase_ = document.getElementById('purchase_');
+let product_num = document.getElementById('product_num');
+let spend = document.getElementById('spend');
+let units = document.getElementById('units');
+let store_r = document.getElementById('store_r');
+let week_num = document.getElementById('week_num');
+let year = document.getElementById('year');
+let uploadButton = document.getElementById('uploadButton');
+
+//to be finished
